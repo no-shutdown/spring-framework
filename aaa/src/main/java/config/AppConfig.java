dcc023931990ebc0e52005319addc7188b1f239c
xinlin.huang <xinlin.huang@gitlab.bt.com>
2023-05-04 11:53:26 +0800

    整合mybatis-spring

--- aaa/src/main/java/config/AppConfig.java
+++ aaa/src/main/java/config/AppConfig.java
@@ -1,11 +1,15 @@
 package config;
 
 import entity.BeanUser;
+import org.apache.ibatis.datasource.DataSourceFactory;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.core.io.ClassPathResource;
 import pojo.ImportUser;
@@ -14,6 +18,7 @@ import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author xinLin.huang
@@ -42,8 +47,13 @@ public class AppConfig {
 	@Bean
 	public SqlSessionFactoryBean sqlSessionFactoryBean() {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
-		sqlSessionFactoryBean.setDataSource(new DataSource() {});
+		org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+		// Configuration的环境，数据源，事务管理器配置会被忽略
+		// 取而代之的是SqlSessionFactoryBean对应的environment，dataSource，transactionFactory属性
+//		configuration.setEnvironment(new Environment("prod", new SpringManagedTransactionFactory(), new PooledDataSource()));
+		sqlSessionFactoryBean.setConfiguration(configuration);
+		sqlSessionFactoryBean.setDataSource(new PooledDataSource());//配置自己的数据源
+		sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
 		return sqlSessionFactoryBean;
 	}
 
