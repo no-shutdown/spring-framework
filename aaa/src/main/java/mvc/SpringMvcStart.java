dfce1f380540adf595788c3f0acc8eb3fd4cf97a
xinlin.huang <xinlin.huang@gitlab.bt.com>
2023-07-25 18:03:24 +0800

    spring mvc start

--- aaa/src/main/java/mvc/SpringMvcStart.java
+++ aaa/src/main/java/mvc/SpringMvcStart.java
@@ -1,12 +1,8 @@
 package mvc;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
-
-import java.io.File;
 
 /**
  * @author xinLin.huang
@@ -15,20 +11,15 @@ import java.io.File;
 
 public class SpringMvcStart {
 
-	public static void main(String[] args) throws LifecycleException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public static void main(String[] args) throws LifecycleException {
 
 		Tomcat tomcat = new Tomcat();
 
-
-//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");//设置协议
 		Connector connector = new Connector();
 		connector.setPort(8081);//设置端口
 		tomcat.getService().addConnector(connector);
 
 		tomcat.addWebapp("/", System.getProperty("java.io.tmpdir"));
-
-//		Context context = tomcat.addContext("/", System.getProperty("java.io.tmpdir"));
-//		context.addLifecycleListener((LifecycleListener) Class.forName(tomcat.getHost().getConfigClass()).newInstance());
 
 		tomcat.start();
 		tomcat.getServer().await();
--- aaa/src/main/java/mvc/config/MvcConfig.java
+++ aaa/src/main/java/mvc/config/MvcConfig.java
@@ -1,8 +1,19 @@
 package mvc.config;
 
+import mvc.interceptors.MvcInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author xinLin.huang
@@ -12,5 +23,49 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan({"mvc.controller"})
 @EnableWebMvc
 public class MvcConfig {
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfig();
+	}
+
+	static class WebMvcConfig implements WebMvcConfigurer {
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry.addInterceptor(new MvcInterceptor());
+		}
+	}
+
+
+
+
+	//这种方式单纯只用springMvc的话是不会自动配置到servlet容器中的，但是springBoot可以自动配置
+	@Bean
+	public WebMvcConfigurationSupport webMvcConfigurationSupport() {
+		return new WebMvcConfigurationSupport() {
+			@Override
+			protected void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(new HandlerInterceptor() {
+					@Override
+					public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+						System.out.println("webMvcConfigurationSupport");
+						return HandlerInterceptor.super.preHandle(request, response, handler);
+					}
+				});
+			}
+		};
+	}
+	//这种方式单纯只用springMvc的话是不会自动配置到servlet容器中的，但是springBoot可以自动配置
+	@Bean
+	public Filter filter() {
+		return new Filter() {
+			@Override
+			public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+				System.out.println("myFilter");
+				chain.doFilter(request, response);
+			}
+		};
+	}
+
 
 }
